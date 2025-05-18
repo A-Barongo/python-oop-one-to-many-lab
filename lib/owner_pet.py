18772ca8552446f9388b168a90fdb159f78a5767
Allan <allanbarongo5@gmail.com>
2025-05-18 16:59:34 +0300

    python one to many object relationship

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,30 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all=[]
+    def __init__(self,name,pet_type,owner=None):
+        self.name=name
+        self.pet_type=pet_type
+        self.owner=owner
+        Pet.all.append(self)
+    @property
+    def pet_type(self):
+        return self._pet_type
+    @pet_type.setter
+    def pet_type(self,value):
+        if value in Pet.PET_TYPES:
+            self._pet_type=value
+        else:
+            raise Exception
 
 class Owner:
-    pass
+    def __init__(self,name):
+        self.name=name
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner==self]
+    def add_pet(self,pet):
+        if isinstance(pet,Pet):
+            pet.owner=self
+        else:
+            raise Exception
+    def get_sorted_pets(self):
+        return sorted(Pet.all, key=lambda pet: pet.name)
